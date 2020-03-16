fc4f686208377920807bdcada45bd81d2fbc6ed7
Deyan-Petkov <deyan.post@gmail.com>
2020-03-16 11:05:56 +0000

    Added Database connectivity, Login functionality - recognising user and user type credentials, Class names changed to capital letters according to the naming conventions standard.

---
+++ JavaApplication/README.txt
@@ -0,0 +1,5 @@
+Added functionalities :
+
+1. Database connectivity
+2. Login functionality - recognising user and user type credentials  
+3. Class names changed to capital letters according to the naming conventions standard.
---
+++ JavaApplication/src/main/java/DbCon.java
@@ -0,0 +1,32 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author deyan
+ */
+public class DbCon {
+    //path to the database file
+    private static final String SQCONN = "jdbc:sqlite:AirLink.db";
+    
+    public static Connection getConnection() throws ClassNotFoundException{
+        try {
+            return DriverManager.getConnection(SQCONN);//Allocates the appropriate driver for the database and creates connection
+        } catch (SQLException ex) {
+            Logger.getLogger(DbCon.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return null;
+    }
+    
+}
