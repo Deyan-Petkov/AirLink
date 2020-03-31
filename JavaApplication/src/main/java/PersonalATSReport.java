07e2c1ecc091dab986f6f2b32ed6ebfcc3c15f44
DjilasMito <56593826+DjilasMito@users.noreply.github.com>
2020-03-31 15:38:09 +0100

    Added GlobalInterlineReport, InterlineSalesReport, DomesticSalesReport, AdvisorReportList, PersonalInterlineReport

--- JavaApplication/src/main/java/PersonalATSReport.java
+++ JavaApplication/src/main/java/PersonalATSReport.java
@@ -41,7 +41,7 @@ public class PersonalATSReport extends javax.swing.JFrame {
         
         if (date.matches("([0-9]{4})-([0-9]{2})-([0-9]{2})")) {
             return true;
-        }
+    }
         return false;
     }
     
@@ -447,23 +447,18 @@ public class PersonalATSReport extends javax.swing.JFrame {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(PersonalATSReport.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(PersonalATSReport.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(PersonalATSReport.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(PersonalATSReport.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
         //</editor-fold>
+        
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new PersonalATSReport().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new PersonalATSReport().setVisible(true);
         });
     }
 
